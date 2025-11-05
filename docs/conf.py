4ac2cc52c08e00175bf08bd58ef70487476af63b
Nadim-Daniel Ghaznavi <nghaznavi@gmail.com>
2025-11-05 10:12:51 -0500

    Release v1.2.10

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "SystemCtl"
 copyright = "2025, Nadim-Daniel Ghaznavi"
 author = "Nadim-Daniel Ghaznavi"
-release = "1.2.9"
+release = "1.2.10"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
